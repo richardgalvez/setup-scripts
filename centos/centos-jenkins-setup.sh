bc42a1dde41ab902d916b77bc21961e93865f689
Richard G <@.com>
2022-01-13 21:52:43 -0500

    Updated Jenkins setup script for CentOS.

--- centos/centos-jenkins-setup.sh
+++ centos/centos-jenkins-setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-# This will install EPEL to enable packages similar to CentOS for Red Hat Linux and install the dependcies a Jenkins lab.
+# This will install EPEL to enable packages similar to CentOS for Red Hat Linux and install the dependcies for a Jenkins lab.
 
-# Don't forget to open TCP port 8080 (default Jenkins port) in the Security group!
+# Opens TCP port 8080 (default Jenkins port) in the Security group.
+sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+sudo firewall-cmd --reload
 
-sudo yum install wget -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install java-11-openjdk-devel jenkins -y
+sudo yum upgrade -y
+sudo yum install epel-release java-11-openjdk-devel -y 
+sudo yum install jenkins -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
